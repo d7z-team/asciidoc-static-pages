57d30da0c4639f4be326beb7ecd11a1a6c88d539
OpenEdgn <ExplodingFKL@gmail.com>
2022-08-04 22:26:55 +0800

    feat: 完成项目

--- script/setup-deb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ ! "$1" ]; then
-    echo "你需要指定文档位置"
-    exit 1
-fi
-docker run -it --rm -v $(pwd):/documents ghcr.io/d7z-team/asciidoc-static-pages:v2  pages -c /documents/.pages.yaml --output public
-echo "output dir: $(pwd)/public"
