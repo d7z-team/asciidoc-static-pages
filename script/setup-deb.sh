78038b818551edc88390b119bd1a457250b19601
Dragon Exploding <kailong.fu@powersi.com>
2022-08-04 11:55:53 +0800

    feat: 更新打包配置

--- script/setup-deb.sh
+++ script/setup-deb.sh
@@ -3,4 +3,5 @@ if [ ! "$1" ]; then
     echo "你需要指定文档位置"
     exit 1
 fi
-docker run -it --rm -v $(pwd):/documents ghcr.io/d7z-team/asciidoc-static-pages:v2  "pages -c /documents/.pages.yaml --output public"
+docker run -it --rm -v $(pwd):/documents ghcr.io/d7z-team/asciidoc-static-pages:v2  pages -c /documents/.pages.yaml --output public
+echo "output dir: $(pwd)/public"
